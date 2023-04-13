477d946bcc9ecc04251539af854930eea16ec1b5
Miron Dima <mirontsov2098@mail.ru>
2023-04-13 13:25:12 +0300

    add arrowup

---
+++ src/modules/up.js
@@ -0,0 +1,32 @@
+const arrowUp = () => {
+	const arrowUp = document.querySelector('.up')
+	
+	const hiddenUp = () => {
+		arrowUp.style.display = 'none'
+	}
+	hiddenUp()
+	const showUp = () => {
+		arrowUp.style.display = 'block'
+	}
+
+	document.addEventListener('scroll', () => {
+		let scrollY = window.scrollY
+		console.log(scrollY);
+		if (scrollY > 450) {
+			showUp()
+		}
+		else if (scrollY < 450) {
+			hiddenUp()
+		}
+	})
+
+	arrowUp.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: 'smooth'
+		  })
+	})
+}
+
+export default arrowUp
--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ import  modal  from "./modules/modal";
 import scroll from './modules/scroll'
 import swiper from './modules/swiper'
 import carusel from './modules/carusel'
+import arrowUp from './modules/up'
 
+arrowUp()
 modal()
 scroll()
 swiper()
